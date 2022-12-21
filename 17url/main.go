e76bbb0fa812d7645d781f10431a1cf5f7a1b120
unknown <shah10zeb@gmail.com>
2022-12-22 00:01:13 +0530

    server

--- 17url/main.go
+++ 17url/main.go
@@ -12,15 +12,27 @@ func main() {
 	fmt.Println(myurl)
 	result, _ := url.Parse(myurl)
 	fmt.Println(result)
+	//.Scheme = protocol
+	//.Port() = port number
+	fmt.Println(result.Port())
 	fmt.Println(result.Host)
 	fmt.Println(result.Path)
 	fmt.Println(result.RawQuery)
 	qparam := result.Query()
-	fmt.Printf("TYPE of QUERRY PARAMS ARE : %T ", qparam)
+	fmt.Printf("TYPE of QUERRY PARAMS ARE : %T \n", qparam)
+	// url.Values fancy name for key values
 	fmt.Println(qparam["coursename"])
 
 	for _, val := range qparam {
 		fmt.Println("PARAM : ", val)
 	}
 
+	partsOfUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "abc.com",
+		Path:    "/pathA",
+		RawPath: "user=Shahzeb",
+	}
+	anotherURL := partsOfUrl.String()
+	fmt.Println(anotherURL)
 }
